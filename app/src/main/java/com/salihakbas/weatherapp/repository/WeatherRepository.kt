412d610d1912cf746b7f72aa776edb8abf36cfcd
Salih Akbas <salihhakbass@gmail.com>
2024-04-30 12:40:34 +0300

    forecast load and get functions added

--- app/src/main/java/com/salihakbas/weatherapp/repository/WeatherRepository.kt
+++ app/src/main/java/com/salihakbas/weatherapp/repository/WeatherRepository.kt
@@ -6,4 +6,7 @@ class WeatherRepository(val api: ApiServices) {
 
     fun getCurrentWeather(lat:Double,lng:Double,unit:String) =
         api.getCurrentWeather(lat,lng,unit,"e7cee661dd30ea60b6ce1782353360da")
+
+    fun getForecastWeather(lat:Double,lng:Double,unit:String) =
+        api.getForecastWeather(lat,lng,unit,"e7cee661dd30ea60b6ce1782353360da")
 }
