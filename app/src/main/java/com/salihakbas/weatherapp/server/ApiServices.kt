a975f18044bdbbfb31c0735322c4bea3bbb102f2
Salih Akbas <salihhakbass@gmail.com>
2024-04-30 12:41:05 +0300

    get forecast function added apiService

--- app/src/main/java/com/salihakbas/weatherapp/server/ApiServices.kt
+++ app/src/main/java/com/salihakbas/weatherapp/server/ApiServices.kt
@@ -1,6 +1,7 @@
 package com.salihakbas.weatherapp.server
 
 import com.salihakbas.weatherapp.model.CurrentResponseApi
+import com.salihakbas.weatherapp.model.ForecastResponseApi
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,4 +16,14 @@ interface ApiServices {
         @Query("appid") ApiKey: String,
 
     ): Call<CurrentResponseApi>
+
+
+    @GET("data/2.5/weather")
+    fun getForecastWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") units: String,
+        @Query("appid") ApiKey: String,
+
+        ): Call<ForecastResponseApi>
 }
